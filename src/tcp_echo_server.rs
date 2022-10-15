0867645bc64cffe1b063a984eb24318a4d7c161b
Nick Loadholtes <nick@ironboundsoftware.com>
2022-10-15 12:58:27 -0400

    rustfmt

--- src/tcp_echo_server.rs
+++ src/tcp_echo_server.rs
@@ -1,4 +1,3 @@
-
 // From a book with some sample rust code for dealing with sockets
 //
 // for use with https://protohackers.com/problem/0 perhaps?
@@ -6,7 +5,7 @@
 use std::net::{TcpListener, TcpStream};
 use std::thread;
 
-use std::io::{Read, Write, Error};
+use std::io::{Error, Read, Write};
 
 // Handles a single client
 fn handle_client(mut stream: TcpStream) -> Result<(), Error> {
@@ -14,21 +13,23 @@ fn handle_client(mut stream: TcpStream) -> Result<(), Error> {
     let mut buf = [0; 512];
     loop {
         let bytes_read = stream.read(&mut buf)?;
-        if bytes_read == 0 { return Ok(()); }
+        if bytes_read == 0 {
+            return Ok(());
+        }
         stream.write(&buf[..bytes_read])?;
     }
 }
 
 fn echo_server_main() {
-    let listener = TcpListener::bind("0.0.0.0:8888")
-                               .expect("Could not bind");
+    let listener = TcpListener::bind("0.0.0.0:8888").expect("Could not bind");
     for stream in listener.incoming() {
         match stream {
-            Err(e) => { eprintln!("failed: {}", e) }
+            Err(e) => {
+                eprintln!("failed: {}", e)
+            }
             Ok(stream) => {
                 thread::spawn(move || {
-                    handle_client(stream)
-                    .unwrap_or_else(|error| eprintln!("{:?}", error));
+                    handle_client(stream).unwrap_or_else(|error| eprintln!("{:?}", error));
                 });
             }
         }
